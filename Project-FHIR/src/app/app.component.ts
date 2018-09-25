303d592a760b0719fc38931b6f9490257381597c
houledmo <hamza.ouledmoussa@gmail.com>
2018-09-25 11:50:50 +0200

    Quatrieme Commit: Commandes get + params

--- Project-FHIR/src/app/app.component.ts
+++ Project-FHIR/src/app/app.component.ts
@@ -17,4 +17,11 @@ export class AppComponent {
           console.log(value);
       });
   }
+
+  getAppointment() {
+    this.dataService.getAppointment()
+      .subscribe((value ) => {
+          console.log(value);
+      });
+  }
 }
--- Project-FHIR/src/services/data-service.service.ts
+++ Project-FHIR/src/services/data-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
 @Injectable({
@@ -8,8 +8,19 @@ import { environment } from '../environments/environment';
 export class DataServiceService {
 
   constructor(private http: HttpClient) { }
+/*   params = new HttpParams().append('participant.actor.reference', 'Patient/5ba8b75b2eef950010bbb5b1');
+ */  params = new HttpParams().append('participant.actor.reference', 'Practitioner/5ba8b7742eef950010bbb5b3');
+
+
+
 
   getPractData() {
     return this.http.get(environment.baseUrl + 'practitioner/' + environment.practId);
   }
+
+  getAppointment() {
+    return this.http.get(environment.baseUrl + 'appointment', {params: this.params});
+  }
+
+
 }
