3dd9028b68ab3dd2fee94f48e30ff5548de70f48
houledmo <hamza.ouledmoussa@gmail.com>
2018-09-25 12:29:11 +0200

    Commit Ajout des routes

--- Project-FHIR/src/app/app.module.ts
+++ Project-FHIR/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DataServiceService } from '../services/data-service.service';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PractitionerComponent } from './practitioner/practitioner.component';
 import { AppointmentComponent } from './appointment/appointment.component';
 import { ObservationComponent } from './observation/observation.component';
+
+const appRoutes: Routes = [
+  { path: 'pract', component: PractitionerComponent },
+  { path: 'obs',      component: ObservationComponent },
+  { path: 'appoint',      component: AppointmentComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,9 +25,12 @@ import { ObservationComponent } from './observation/observation.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    RouterModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
