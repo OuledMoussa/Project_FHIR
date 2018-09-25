cb55be5e44736f758ca295f6b0c641d57f5fa8c5
houledmo <hamza.ouledmoussa@gmail.com>
2018-09-25 10:58:33 +0200

    Troisieme Commit: Ajout des components

--- Project-FHIR/src/app/app.module.ts
+++ Project-FHIR/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { DataServiceService } from '../services/data-service.service';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PractitionerComponent } from './practitioner/practitioner.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { ObservationComponent } from './observation/observation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PractitionerComponent,
+    AppointmentComponent,
+    ObservationComponent
   ],
   imports: [
     BrowserModule,
