c23d9c0f3e6facd6c0750f15c17dce9d9d41e1b0
Nocturc <antoine.nguyen_van@etud.univ-jfc.fr>
2018-09-26 15:10:11 +0200

    obs

--- Project-FHIR/src/app/observation/observation.component.ts
+++ Project-FHIR/src/app/observation/observation.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from '../../services/data-service.service';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 @Component({
   selector: 'app-observation',
   templateUrl: './observation.component.html',
@@ -11,8 +19,24 @@ export class ObservationComponent implements OnInit {
 
   ngOnInit() {
     this.dataService.getPatients().subscribe((value) => {
-      console.log(value);
         this.patientsArray = value });
   }
-
+  checker = 0;
+  showGroup(x) {
+    switch(x){
+      case 'tryg':
+        console.log("test");
+        this.checker = 1;
+        break;
+      case 'chol':
+        this.checker =  2;
+        break;
+      case 'gen':
+        this.checker =  3;
+        break;
+      default:
+        this.checker =  0;
+        break;
+    }
+  }
 }
